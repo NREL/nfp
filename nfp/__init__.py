11fed3dd1d2f0dc697623fc28eabc3a8be88f44f
Peter St. John <peterc.stjohn@gmail.com>
2019-01-04 19:39:36 -0700

    Adding MAE loss to custom objects
    Former-commit-id: 4dbdd593e63f884e1ca770d7ad7256a948d2bf73

--- nfp/__init__.py
+++ nfp/__init__.py
@@ -11,5 +11,6 @@ custom_layers = {
     'Embedding2D': Embedding2D,
     'Squeeze': Squeeze,
     'GraphModel': GraphModel,
-    'masked_mean_squared_error': masked_mean_squared_error
+    'masked_mean_squared_error': masked_mean_squared_error,
+    'masked_mean_absolute_error': masked_mean_absolute_error
 }

b9d1b2ead6bb26e5eb9342c5d55cfd7735428724
Peter St. John <peterc.stjohn@gmail.com>
2019-01-10 14:59:45 -0700

    pytest was pulling in keras tests
    Former-commit-id: 8ed73cfdbe63f42e3b0727ed044206ca3daea7c2

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     ],
 
     packages=find_packages(exclude=['docs', 'tests']),  # Required
-    install_requires=['numpy', 'tqdm', 'keras', 'tensorflow', 'tqdm'],
+    install_requires=['numpy', 'tqdm', 'keras', 'tensorflow', 'tqdm', 'scikit-learn'],
 
     project_urls={
         'Source': 'https://github.com/NREL/nfp',

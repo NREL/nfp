87b7bb02e8a5d15ccc77aafbf0693d61159e27ea
Peter St. John <peterc.stjohn@gmail.com>
2019-04-16 08:23:42 -0600

    adding masked logcosh loss

--- nfp/models/losses.py
+++ nfp/models/losses.py
@@ -4,6 +4,7 @@ values in both the test and predicted tensors when computing the cost. """
 
 import keras.backend as K
 import tensorflow as tf
+import keras.losses
 
 def masked_mean_squared_error(y_true, y_pred):
     mask = tf.is_finite(y_true)
@@ -17,3 +18,10 @@ def masked_mean_absolute_error(y_true, y_pred):
     y_true_mask = tf.boolean_mask(y_true, mask)
     y_pred_mask = tf.boolean_mask(y_pred, mask)
     return K.mean(K.abs(y_pred_mask - y_true_mask), axis=-1)
+
+
+def masked_log_cosh(y_true, y_pred):
+    mask = tf.is_finite(y_true)
+    y_true_mask = tf.boolean_mask(y_true, mask)
+    y_pred_mask = tf.boolean_mask(y_pred, mask)
+    return keras.losses.logcosh(y_true_mask, y_pred_mask)

8ca090c7f98d264350604f0047f19a6a4f43ef1a
Peter St. John <peterc.stjohn@gmail.com>
2020-12-30 07:24:58 -0700

    bumping version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 # Fields marked as "Optional" may be commented out.
 setup(
     name='nfp',
-    version='0.0.8',
+    version='0.0.9',
     description='Keras layers for machine learning on molecular structure',
     long_description=long_description,
     long_description_content_type='text/markdown',

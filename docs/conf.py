a4bfb3812542476db4fe2b4d5932b478ffdac832
Peter St. John <peterc.stjohn@gmail.com>
2021-03-09 09:32:43 -0700

    path editing in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@
 import re
 import os
 
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------

333322ef3cfa89a4b550f9c000822878adf13b7f
Peter St. John <peterc.stjohn@gmail.com>
2021-03-09 09:39:15 -0700

    mocking imports in sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -64,3 +64,4 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 autodoc_typehints = "description"
+autodoc_mock_imports = ["tensorflow", "rdkit"]

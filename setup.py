646855df8940cd0a7f0f3df4efd3caf734a86f84
Peter St. John <peterc.stjohn@gmail.com>
2022-04-27 09:49:01 -0600

    changes for python 36 (#23)
    * changes for python 36
    
    * Update mol_preprocessor.py

--- setup.py
+++ setup.py
@@ -8,44 +8,37 @@ import versioneer
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 setup(
-    name='nfp',
+    name="nfp",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='Keras layers for machine learning on graph structures',
+    description="Keras layers for machine learning on graph structures",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/NREL/nfp',  # Optional
-    author='Peter St. John',
-    author_email='peter.stjohn@nrel.gov',  # Optional
+    long_description_content_type="text/markdown",
+    url="https://github.com/NREL/nfp",  # Optional
+    author="Peter St. John",
+    author_email="peter.stjohn@nrel.gov",  # Optional
     classifiers=[
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-
+        "Intended Audience :: Developers",
         # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-
-    packages=find_packages(exclude=['docs', 'tests']),  # Required
-
+    packages=find_packages(exclude=["docs", "tests"]),  # Required
     project_urls={
-        'Source': 'https://github.com/NREL/nfp',
+        "Source": "https://github.com/NREL/nfp",
     },
 )
